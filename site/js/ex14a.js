d6efb30812bab6ed2e3904f28365277aa2ac92f9
Erynth <20137582@s.ubaguio.edu>
2021-05-15 22:31:17 +0800

    asdadasd

---
+++ site/js/ex14a.js
@@ -0,0 +1,5 @@
+document.getElementById("clean").innerHTML="ERYNTH V EILINGER";
+document.getElementById("clean1").innerHTML="BS COMPUTER SCIENCE";
+document.getElementById("clean2").innerHTML="1st YEAR";
+document.getElementById("clean3").innerHTML="HUMCOM1";
+document.getElementById("clean4").innerHTML="HUMAN-COMPUTER INTERACTION.";
